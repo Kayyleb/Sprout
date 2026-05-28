c1961fe3cd9eb0e30642d67effb810a983f0f4fb
Kayleb Cook <kaylebjuliancook@gmail.com>
2026-05-28 11:59:33 -0700

    Currently moving from sandbox into backend, plaid config first

---
+++ backend/plaid_client.py
@@ -0,0 +1,21 @@
+# File: Plaid_Client
+# This file will read from the secret env file and configure the plaid client to my plaid sandbox
+
+import os 
+import plaid
+from plaid.api import plaid_api
+from dotenv import load_dotenv
+from pathlib import path
+
+load_dotenv(dotenv_path=Path(__file__).resolve().parent.parent / ".env")
+
+# ── Configure Plaid client ──────────────────────────────────────────────────
+configuration = plaid.Configuration(
+    host=plaid.Environment.Sandbox,
+    api_key={
+        "clientId": os.getenv("PLAID_CLIENT_ID"),
+        "secret":   os.getenv("PLAID_SECRET"),
+    }
+)
+api_client = plaid.ApiClient(configuration)
+client = plaid_api.PlaidApi(api_client)
